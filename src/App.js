638458b5f38bab9029e3cd72888e3330e054549e
Arnaud Tussy-Vassilieff <atussy-vassilieff@supralog.com>
2021-11-06 18:10:54 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import tailwingLogo from './tailwindcss.svg';
+import reactLogo from './logo.svg';
 
 function App() {
+
+  const logoSize = '250px';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="flex w-full h-screen items-center justify-center bg-gray-50">
+      <div className="flex flex-col items-center">
+        <a href="https://reactjs.org/" target="_blank">
+          <img className="animate-spin-slow" style={{width: logoSize}} src={reactLogo} alt="React logo" />
         </a>
-      </header>
+        <p className="text-4xl font-bold">+</p>
+        <a href="https://tailwindcss.com/" target="_blank">
+          <img className="animate-bounce" style={{width: logoSize}} src={tailwingLogo} alt="Tailwind CSS logo"/>
+        </a>
+        <p className="text-4xl font-bold">=</p>
+        <p className="text-4xl mt-5 animate-ping">💖</p>
+      </div>      
     </div>
   );
 }
